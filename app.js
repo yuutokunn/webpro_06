59c8a83c953fd38839d5c827ed2285b417d13619
yuutokunn <thesabawn@icloud.com>
2026-02-09 00:02:20 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -6,27 +6,50 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 
-let zisyuuSpots = [{ id: 1, name: "図書館5F", location: "5号館", status: "空き" }];
-let cafeterias = [{ id: 1, shop: "津田沼食堂", menu: "カツカレー", price: 450 }];
-let courses = [{ id: 1, name: "Webプログラミング", time: "月2", room: "231" }];
+let data = {
+    zisyuu: [{ id: 1, name: "図書館5F", location: "5号館", status: "空き" }],
+    cafe: [{ id: 1, name: "木", location: "塩豚丼", status: "350円" }],
+    risyuu: [{ id: 1, name: "月2", location: "Webプログラミング", status: "231教室" }]
+};
 let nextId = 100;
 
 
-app.get("/zisyuu", (req, res) => res.render("zisyuu/list", { items: zisyuuSpots }));
-app.post("/zisyuu/add", (req, res) => {
-    zisyuuSpots.push({ 
-        id: nextId++, 
-        name: req.body.name, 
-        location: req.body.location, 
-        status: req.body.status 
+const setupRoutes = (path, key) => {
+    
+    app.get("/" + path, (req, res) => {
+        res.render(path + "/list", { items: data[key], mode: "index", target: null });
     });
-    res.redirect("/zisyuu"); 
-});
+   
+    app.get("/" + path + "/detail/:id", (req, res) => {
+        const item = data[key].find(i => i.id === parseInt(req.params.id));
+        res.render(path + "/list", { items: data[key], mode: "detail", target: item });
+    });
+    
+    app.get("/" + path + "/edit/:id", (req, res) => {
+        const item = data[key].find(i => i.id === parseInt(req.params.id));
+        res.render(path + "/list", { items: data[key], mode: "edit", target: item });
+    });
+    
+    app.post("/" + path + "/add", (req, res) => {
+        data[key].push({ id: nextId++, ...req.body });
+        res.redirect("/" + path);
+    });
+   
+    app.post("/" + path + "/update/:id", (req, res) => {
+        const index = data[key].findIndex(i => i.id === parseInt(req.params.id));
+        if (index !== -1) data[key][index] = { id: parseInt(req.params.id), ...req.body };
+        res.redirect("/" + path);
+    });
+    
+    app.post("/" + path + "/delete/:id", (req, res) => {
+        data[key] = data[key].filter(i => i.id !== parseInt(req.params.id));
+        res.redirect("/" + path);
+    });
+};
 
 
-app.get("/cafe", (req, res) => res.render("cafe/list", { items: cafeterias }));
-app.post("/cafe/add", (req, res) => { cafeterias.push({ id: nextId++, ...req.body }); res.redirect("/cafe"); });
-app.get("/risyuu", (req, res) => res.render("risyuu/list", { items: courses }));
-app.post("/risyuu/add", (req, res) => { courses.push({ id: nextId++, ...req.body }); res.redirect("/risyuu"); });
+setupRoutes("zisyuu", "zisyuu");
+setupRoutes("cafe", "cafe");
+setupRoutes("risyuu", "risyuu");
 
 module.exports = app;
